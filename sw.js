8a8209d6e205e140977e07cc7c457aaf78e59dee
Jackson Slater <jackson@Jacksons-MacBook-Pro-9.local>
2026-04-06 15:53:45 -0600

    Initial commit: morning desk PWA

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'morning-desk-v1';
+const SHELL = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+
+  // Always hit network for weather API and fonts
+  if (url.hostname !== self.location.hostname) {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
+  // Cache-first for app shell
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+    )
+  );
+});
